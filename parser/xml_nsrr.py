4fe352bd258831abffb532c436868e54b9ccb825
Blake Macnair <blakemacnair@gmail.com>
2019-03-02 13:01:04 -0600

    Add parser for getting sleep stages from NSRR-formatted XML files.

--- parser/xml_nsrr.py
+++ parser/xml_nsrr.py
@@ -1,24 +1,24 @@
 from xml.etree import ElementTree
 
 
-def parse_xml_annotations(file_path):
+def parse_nsrr_annotations(file_path):
     tree = ElementTree.parse(file_path)
     root = tree.getroot()
     return root
 
 
-def parse_xml_scored_events(file_path):
-    root = parse_xml_annotations(file_path)
+def parse_nsrr_scored_events(file_path):
+    root = parse_nsrr_annotations(file_path)
     scored_events = root.find('ScoredEvents').getchildren()
     return scored_events
 
 
-def parse_xml_sleep_stage_events(file_path):
-    events = parse_xml_scored_events(file_path)
+def parse_nsrr_sleep_stages(file_path):
+    events = parse_nsrr_scored_events(file_path)
     sleep_stages = [event for event in events if event.find('EventType').text == 'Stages|Stages']
     return sleep_stages
 
 
 if __name__ == "__main__":
-    file_path = "/Users/blakemacnair/dev/data/shhs/polysomnography/annotations-events-nsrr/shhs1/shhs1-200001-nsrr.xml"
-    result = parse_xml_sleep_stage_events(file_path)
+    file_path = "./dev/data/shhs/polysomnography/annotations-events-nsrr/shhs1/shhs1-200001-nsrr.xml"
+    result = parse_nsrr_sleep_stages(file_path)
