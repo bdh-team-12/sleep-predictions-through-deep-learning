02200833fb65b191296f74d384084cd211bc2e29
Blake Macnair <blakemacnair@gmail.com>
2019-03-02 10:27:57 -0600

    Refactor the batch learning code into broken down functions.

--- mne-demo/plot_sleep_batch.py
+++ mne-demo/plot_sleep_batch.py
@@ -1,16 +1,13 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 import mne
 from mne.datasets.sleep_physionet.age import fetch_data
 from mne.time_frequency import psd_array_welch
 
+from statistics import mean
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import FunctionTransformer
 from sklearn.model_selection import train_test_split
 
 ALL_SUBJECTS = list(range(20))
@@ -21,18 +18,19 @@ mapping = {'EOG horizontal': 'eog',
            'Temp rectal': 'misc',
            'Event marker': 'misc'}
 
-# With ALL data
-all_files_first_night = fetch_data(subjects=ALL_SUBJECTS, recording=[1])
-all_raw_edf = []
+FREQ_BANDS = {"delta": [0.5, 4.5],
+                  "theta": [4.5, 8.5],
+                  "alpha": [8.5, 11.5],
+                  "sigma": [11.5, 15.5],
+                  "beta": [15.5, 30]}
 
-for file in all_files_first_night:
-    raw = mne.io.read_raw_edf(file[0])
-    annot = mne.read_annotations(file[1])
+EEG_CHANNELS = ["EEG Fpz-Cz", "EEG Pz-Oz"]
 
-    raw.set_annotations(annot, emit_warning=False)
-    raw.set_channel_types(mapping)
-
-    all_raw_edf.append(raw)
+event_id = {'Sleep stage W': 1,
+            'Sleep stage 1': 2,
+            'Sleep stage 2': 3,
+            'Sleep stage 3/4': 4,
+            'Sleep stage R': 5}
 
 annotation_desc_2_event_id = {'Sleep stage W': 1,
                               'Sleep stage 1': 2,
@@ -41,23 +39,42 @@ annotation_desc_2_event_id = {'Sleep stage W': 1,
                               'Sleep stage 4': 4,
                               'Sleep stage R': 5}
 
-all_events = []
-for raw_sample in all_raw_edf:
-    events, _ = mne.events_from_annotations(raw_sample, event_id=annotation_desc_2_event_id, chunk_duration=30.)
-    all_events.append(events)
 
-event_id = {'Sleep stage W': 1,
-            'Sleep stage 1': 2,
-            'Sleep stage 2': 3,
-            'Sleep stage 3/4': 4,
-            'Sleep stage R': 5}
+def fetch_subjects_from_sleep_physionet():
+    all_files_first_night = fetch_data(subjects=ALL_SUBJECTS, recording=[1])
+    all_raw_edf = []
 
-all_epochs = []
-for (raw, events) in zip(all_raw_edf, all_events):
-    t_max = 30. - 1. / raw.info['sfreq']
-    epochs = mne.Epochs(raw=raw, events=events, event_id=event_id,
-                        tmin=0, tmax=t_max, baseline=None)
-    all_epochs.append(epochs)
+    for file in all_files_first_night:
+        raw = mne.io.read_raw_edf(file[0])
+        annot = mne.read_annotations(file[1])
+
+        raw.set_annotations(annot, emit_warning=False)
+        raw.set_channel_types(mapping)
+
+        all_raw_edf.append(raw)
+
+    return all_raw_edf
+
+
+def extract_events_from_raw_edfs(raw_edfs):
+    all_events = []
+    for raw_sample in raw_edfs:
+        events, _ = mne.events_from_annotations(raw_sample, event_id=annotation_desc_2_event_id, chunk_duration=30.)
+        all_events.append(events)
+
+    return all_events
+
+
+def extract_epochs(raw_edfs, events):
+    all_epochs = []
+    for (raw, events) in zip(raw_edfs, events):
+        t_max = 30. - 1. / raw.info['sfreq']
+        epochs = mne.Epochs(raw=raw, events=events, event_id=event_id,
+                            tmin=0, tmax=t_max, baseline=None)
+        all_epochs.append(epochs)
+
+    return all_epochs
+
 
 def eeg_power_band(epochs):
     """EEG relative power band feature extraction.
@@ -77,13 +94,6 @@ def eeg_power_band(epochs):
         Transformed data.
     """
     # specific frequency bands
-    FREQ_BANDS = {"delta": [0.5, 4.5],
-                  "theta": [4.5, 8.5],
-                  "alpha": [8.5, 11.5],
-                  "sigma": [11.5, 15.5],
-                  "beta": [15.5, 30]}
-
-    EEG_CHANNELS = ["EEG Fpz-Cz", "EEG Pz-Oz"]
 
     sfreq = epochs.info['sfreq']
     data = epochs.load_data().pick_channels(EEG_CHANNELS).get_data()
@@ -100,30 +110,53 @@ def eeg_power_band(epochs):
     return np.concatenate(X, axis=1)
 
 
-all_data = []
-all_target = []
-for epochs in all_epochs:
-    data = eeg_power_band(epochs)
-    all_data.append(data)
+def generate_power_band_data_from_epochs(epochs):
+    all_data = []
+    all_target = []
+    for epoch in epochs:
+        data = eeg_power_band(epoch)
+        all_data.append(data)
 
-    events = epochs.events
-    targets = events[:, 2]
-    all_target.append(targets)
+        events = epoch.events
+        targets = events[:, 2]
+        all_target.append(targets)
 
-test_size = 5/20
+    return all_data, all_target
 
-X_train, X_test, y_train, y_test = train_test_split(all_data, all_target, test_size=0.4, random_state=0)
 
-rfc = RandomForestClassifier(n_estimators=50, random_state=42, warm_start=True)
+def train_random_forest_classifier(X, y):
+    rfc = RandomForestClassifier(n_estimators=50, random_state=42, warm_start=True)
 
-for (X, y) in zip(X_train, y_train):
-    rfc.fit(X, y)
-    rfc.n_estimators += 50
+    for (X_sample, y_sample) in zip(X, y):
+        rfc.fit(X_sample, y_sample)
+        rfc.n_estimators += 50
 
-all_acc = []
-for (Xt, yt) in zip(X_test, y_test):
-    yp = rfc.predict(Xt)
-    acc = accuracy_score(yt, yp)
-    all_acc.append(acc)
+    return rfc
 
-print("Accuracy scores for cross validation: {}".format(all_acc))
+
+def test_random_forest_classifier(rfc, X, y):
+    all_acc = []
+    for (Xt, yt) in zip(X, y):
+        yp = rfc.predict(Xt)
+        acc = accuracy_score(yt, yp)
+        all_acc.append(acc)
+
+    return all_acc
+
+
+if __name__ == "__main__":
+    raw_edfs = fetch_subjects_from_sleep_physionet()
+    events = extract_events_from_raw_edfs(raw_edfs)
+    epochs = extract_epochs(raw_edfs, events)
+
+    X, y = generate_power_band_data_from_epochs(epochs)
+
+    test_size = 5 / 20
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=0)
+
+    rfc = train_random_forest_classifier(X_train, y_train)
+
+    scores = test_random_forest_classifier(rfc, X_test, y_test)
+
+    print("Accuracy scores for cross validation: {}".format(scores))
+    print("Average accuracy: {}".format(mean(scores)))
