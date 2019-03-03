72b444ce0c8582e5570653f773101b9b5ba4ed5e
Blake Macnair <blakemacnair@gmail.com>
2019-03-02 19:21:14 -0600

    Remove dummy code from the parser files.

--- shhs/parser/xml_nsrr.py
+++ shhs/parser/xml_nsrr.py
@@ -17,8 +17,3 @@ def parse_nsrr_sleep_stages(file_path):
     events = parse_nsrr_scored_events(file_path)
     sleep_stages = [event for event in events if event.find('EventType').text == 'Stages|Stages']
     return sleep_stages
-
-
-if __name__ == "__main__":
-    file_path = "./dev/data/shhs/polysomnography/annotations-events-nsrr/shhs1/shhs1-200001-nsrr.xml"
-    result = parse_nsrr_sleep_stages(file_path)
--- shhs/parser/xml_profusion.py
+++ shhs/parser/xml_profusion.py
@@ -12,8 +12,3 @@ def parse_profusion_sleep_stages(file_path):
     root = parse_profusion_annotations(file_path)
     sleep_stages = root.find('SleepStages').getchildren()
     return sleep_stages
-
-
-if __name__ == "__main__":
-    test_file_path = "./dev/data/shhs/polysomnography/annotations-events-profusion/shhs1/shhs1-200001-profusion.xml"
-    result = parse_profusion_sleep_stages(file_path)
