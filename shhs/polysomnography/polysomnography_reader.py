e7eaf4b648c4d48a3a80a792e9a6ac7305dd148b
Blake Macnair <blakemacnair@gmail.com>
2019-03-09 14:45:12 -0600

    Add the powerband function from the MNE demo to our framework.

--- shhs/polysomnography/polysomnography_reader.py
+++ shhs/polysomnography/polysomnography_reader.py
@@ -54,3 +54,46 @@ def annotated_raw_edf(edf_file_path, annotations_file_path):
     annotations = nsrr_sleep_stage_annotations(annotations_file_path)
     raw.set_annotations(annotations)
     return raw
+
+
+def eeg_power_band(epochs):
+    """EEG relative power band feature extraction.
+
+    This function takes an ``mne.Epochs`` object and creates EEG features based
+    on relative power in specific frequency bands that are compatible with
+    scikit-learn.
+
+    Taken from https://martinos.org/mne/dev/auto_tutorials/plot_sleep.html
+
+    Parameters
+    ----------
+    epochs : Epochs
+        The data.
+
+    Returns
+    -------
+    X : numpy array of shape [n_samples, 5]
+        Transformed data.
+    """
+    # specific frequency bands
+    FREQ_BANDS = {"delta": [0.5, 4.5],
+                  "theta": [4.5, 8.5],
+                  "alpha": [8.5, 11.5],
+                  "sigma": [11.5, 15.5],
+                  "beta": [15.5, 30]}
+
+    EEG_CHANNELS = ["EEG Fpz-Cz", "EEG Pz-Oz"]
+
+    sfreq = epochs.info['sfreq']
+    data = epochs.load_data().pick_channels(EEG_CHANNELS).get_data()
+    psds, freqs = mne.time_frequency.psd_array_welch(data, sfreq, fmin=0.5, fmax=30.,
+                                                     n_fft=512, n_overlap=256)
+    # Normalize the PSDs
+    psds /= np.sum(psds, axis=-1, keepdims=True)
+
+    X = []
+    for _, (fmin, fmax) in FREQ_BANDS.items():
+        psds_band = psds[:, :, (freqs >= fmin) & (freqs < fmax)].mean(axis=-1)
+        X.append(psds_band.reshape(len(psds), -1))
+
+    return np.concatenate(X, axis=1)
