d1fb806973be34d7e11fcf308d45b04e0691ed35
Blake Macnair <blakemacnair@gmail.com>
2019-04-20 22:14:42 -0400

    Try manually adding more module references.

---
+++ sleep-stage/features/__init__.py
@@ -0,0 +1,3 @@
+from .feature_classifier import (SleepStageAutoEncoder,
+                                 generate_feature_classifier_data_loader,
+                                 train_feature_classifier)
